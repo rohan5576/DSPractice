ce01911806b0c9627061d1c7de698405441991dd
Rohan Lodhi <lodhirohan138@gmail.com>
2024-06-21 23:07:11 +0530

    DAY1:Two Sum

---
+++ app/src/main/java/com/example/dspractice/TwoSum.kt
@@ -0,0 +1,26 @@
+package com.example.dspractice
+
+import android.util.Log
+
+//https://leetcode.com/problems/two-sum/description/
+ class Solution {
+    fun twoSum(nums: IntArray?, target: Int): IntArray {
+       val mHashMap  = mutableMapOf<Int,Int>()
+
+        nums?.forEachIndexed { index, i ->
+            mHashMap[i]?.let { return intArrayOf(it,index) }
+            mHashMap[target -i] = index
+        }
+        return intArrayOf()
+    }
+}
+
+fun main(args: Array<String>) {
+
+    val solution = Solution()
+    val nums = intArrayOf(2, 7, 11, 15)
+    val target = 9
+        val result = solution.twoSum(nums, target)
+        println("Indices: [${result[0]}, ${result[1]}]")
+
+}
